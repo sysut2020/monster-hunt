0611df76df13f53e0d7d1f251bfbac5ccc32391c
Emil <emil.nilsen9888@gmail.com>
2020-01-20 13:26:40 +0100

    Code refactor
    Adding comments and structure of the code

--- unityfiles/Assets/Scripts/PlayerMovement.cs
+++ unityfiles/Assets/Scripts/PlayerMovement.cs
@@ -2,25 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 public class PlayerMovement : MonoBehaviour {
-
+    //Character controller script 
     public CharacterController2D controller2D;
-
     public float runSpeed = 40;
-    
+    //Horizontal movement speed of the player
     public float horizontalMove = 7000;
+    //If the player is jumping or not
     public bool jump = false;
-    void Update(){
+    
+    void Update() {
+        //Checks if the player is pressing the right of left arrow key
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         
+        //Checks if the player is pressing up arrow key for jumping
         if (Input.GetButtonDown("Jump")) {
             jump = true;
         }
     }
 
-    private void FixedUpdate(){
+    void FixedUpdate() {
+        //Moves the player accordingly to the pressed arrow keys
         controller2D.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
     }
