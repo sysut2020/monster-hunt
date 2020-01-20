2e35f053bb18c79cdd2a73f1918e92ec7d47bfec
Christoffer Andersen Træen <christoffer.andersen@live.com>
2020-01-20 18:28:09 +0100

    Remove unused field

--- unityfiles/Assets/Scripts/Game/Camera/CameraFollow.cs
+++ unityfiles/Assets/Scripts/Game/Camera/CameraFollow.cs
@@ -14,11 +14,9 @@ using UnityEngine;
 /// two lines in the editor window, which represents the left and right 
 /// clamps.The cameras center point will not go past these points.
 /// </summary>
-[RequireComponent(typeof(Camera))]
+[RequireComponent (typeof (Camera))]
 public class CameraFollow : MonoBehaviour
 {
-
-	private Camera followCamera;
 
 	[SerializeField]
 	private Transform targetToFollow;
@@ -27,27 +25,19 @@ public class CameraFollow : MonoBehaviour
 	/// Adjusts how smooth/slowly the camera will follow the target.
 	/// </summary>
 	[SerializeField]
-	[Range(0, 1)]
+	[Range (0, 1)]
 	private float followSmoothnes;
 
 	// SmoothDamp velocity
 	private Vector3 smoothVelocity = Vector3.zero;
 
 	[SerializeField]
-	[Tooltip("Position on the left side of the scene where the camera should stop.")]
+	[Tooltip ("Position on the left side of the scene where the camera should stop.")]
 	private float leftEdgeClamp = -1;
 
 	[SerializeField]
-	[Tooltip("Position on the right side of the scene where the camera should stop.")]
+	[Tooltip ("Position on the right side of the scene where the camera should stop.")]
 	private float rightEdgeClamp = 1;
-
-	/// <summary>
-	/// Find the camera to controll set it to the local field
-	/// </summary>
-	void Start()
-	{
-		this.followCamera = this.gameObject.GetComponent<Camera>();
-	}
 
 	/// <summary>
 	/// Moves the camera to the target position with smooth damp,
@@ -56,7 +46,7 @@ public class CameraFollow : MonoBehaviour
 	/// Using LateUpdate since the player character is moved in Update; 
 	/// and we now have the new position for the current frame
 	/// </summary>
-	private void LateUpdate()
+	private void LateUpdate ()
 	{
 		Vector3 currentTargetPosition = targetToFollow.position;
 		// Make sure we dont move the camera in Z position.
@@ -64,14 +54,14 @@ public class CameraFollow : MonoBehaviour
 
 		// Dont use Time.deltatime here, 
 		// it is automatically calculated in the function
-		Vector3 newPosition = Vector3.SmoothDamp(
+		Vector3 newPosition = Vector3.SmoothDamp (
 			transform.position,
 			currentTargetPosition,
 			ref smoothVelocity,
 			followSmoothnes);
 
-		this.transform.position = new Vector3(
-			Mathf.Clamp(newPosition.x, this.leftEdgeClamp, this.rightEdgeClamp),
+		this.transform.position = new Vector3 (
+			Mathf.Clamp (newPosition.x, this.leftEdgeClamp, this.rightEdgeClamp),
 			newPosition.y,
 			newPosition.z);
 	}
@@ -80,17 +70,17 @@ public class CameraFollow : MonoBehaviour
 	/// Draws the left and right clamp in editor window, when the 
 	/// object is selected.
 	/// </summary>
-	private void OnDrawGizmosSelected()
+	private void OnDrawGizmosSelected ()
 	{
 		float lineTop = 10 + this.transform.position.y;
 		float lineBottom = -10 + this.transform.position.y;
 
 		Handles.color = Color.magenta;
-		Handles.DrawLine(new Vector3(this.leftEdgeClamp, lineTop, 0), new Vector3(this.leftEdgeClamp, lineBottom, 0));
-		Handles.Label(new Vector3(this.leftEdgeClamp, this.transform.position.y, 0), "LEFT");
+		Handles.DrawLine (new Vector3 (this.leftEdgeClamp, lineTop, 0), new Vector3 (this.leftEdgeClamp, lineBottom, 0));
+		Handles.Label (new Vector3 (this.leftEdgeClamp, this.transform.position.y, 0), "LEFT");
 
-		Handles.DrawLine(new Vector3(this.rightEdgeClamp, lineTop, 0), new Vector3(this.rightEdgeClamp, lineBottom, 0));
-		Handles.Label(new Vector3(this.rightEdgeClamp, this.transform.position.y, 0), "RIGHT");
+		Handles.DrawLine (new Vector3 (this.rightEdgeClamp, lineTop, 0), new Vector3 (this.rightEdgeClamp, lineBottom, 0));
+		Handles.Label (new Vector3 (this.rightEdgeClamp, this.transform.position.y, 0), "RIGHT");
 
 	}
 
