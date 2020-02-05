cfd9e04006b7059dd70e1176c92c037c3b0d854e
Emil Nilsen <emil.nilsen9888@gmail.com>
2020-02-05 20:02:01 +0100

    Character now flips with mouse instead of movement

--- unityfiles/Assets/Scripts/Game/MouseHandler.cs
+++ unityfiles/Assets/Scripts/Game/MouseHandler.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseHandler : MonoBehaviour {
-
+    // Returns a vector from the position that our aim are based of, to the mouse position
     public Vector3 MouseWorldPosition(GameObject aimDirectionPoint) {
         return Camera.main.ScreenToWorldPoint(Input.mousePosition + (Vector3.forward * 10f) -
                                               aimDirectionPoint.transform.position);
+    }
+    
+    // Returns an angle between two vectors in degrees
+    public float AngleBetweenTwoPoints(Vector3 vectorA, Vector3 vectorB) {
+        return Mathf.Atan2(vectorA.y - vectorB.y, vectorA.x - vectorB.x) * Mathf.Rad2Deg;
     }
 }
