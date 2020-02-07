7c3f03b4bdceacdf0a4ee47307f7182899305105
Christoffer A Træen <christoffer.andersen@live.com>
2020-02-07 11:15:59 +0100

    Remove unused constant

--- unityfiles/Assets/Scripts/Game/Player/CharacterController2D.cs
+++ unityfiles/Assets/Scripts/Game/Player/CharacterController2D.cs
@@ -5,15 +5,14 @@ using UnityEngine;
  * This project is only intended to use for educational purposes, not to be
  * sold or re-distriubted to make money.
  */
-public class CharacterController2D : MonoBehaviour
-{
+public class CharacterController2D : MonoBehaviour {
 
 	// Amount of force added when the player jumps.
 	[SerializeField]
 	private float jumpForce = 400f;
-	
+
 	// How much to smooth out the movement
-	[Range (0, .3f)]
+	[Range(0, .3f)]
 	[SerializeField]
 	private float movementSmoothing = .05f;
 
@@ -44,8 +43,6 @@ public class CharacterController2D : MonoBehaviour
 	// Whether or not the player is grounded.
 	private bool grounded;
 
-	// Radius of the overlap circle to determine if the player can stand up
-	const float CEILING_RADIUS = .2f;
 	private Rigidbody2D playerRigidbody2D;
 
 	// For determining which way the player is currently facing.
@@ -54,22 +51,22 @@ public class CharacterController2D : MonoBehaviour
 	private MousePosition mousePosition;
 	private GunAngle gunAngle;
 	private float angle;
-	
-	private void Awake () {
+
+	private void Awake() {
 		playerRigidbody2D = GetComponent<Rigidbody2D>();
 		mousePosition = new MousePosition();
 		gunAngle = new GunAngle();
 	}
 
-	private void FixedUpdate () {
-		Vector3 mouseWorldPosition = mousePosition.MouseWorldPosition (characterCenter);
+	private void FixedUpdate() {
+		Vector3 mouseWorldPosition = mousePosition.MouseWorldPosition(characterCenter);
 		angle = gunAngle.GetGunAngle(transform.position, mouseWorldPosition);
 
 		grounded = false;
 
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
-		Collider2D[] colliders = Physics2D.OverlapCircleAll (groundCheck.position, GROUNDED_RADIUS, whatIsGround);
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, GROUNDED_RADIUS, whatIsGround);
 		for (int i = 0; i < colliders.Length; i++) {
 			if (colliders[i].gameObject != gameObject) {
 				grounded = true;
@@ -77,14 +74,14 @@ public class CharacterController2D : MonoBehaviour
 		}
 	}
 
-	public void Move (float movement, bool crouch, bool jump) {
+	public void Move(float movement, bool crouch, bool jump) {
 		//only control the player if grounded or airControl is turned on
 		if (grounded || airControl) {
 			// Move the character by finding the target velocity
-			Vector3 targetVelocity = new Vector2 (movement * 10f, playerRigidbody2D.velocity.y);
+			Vector3 targetVelocity = new Vector2(movement * 10f, playerRigidbody2D.velocity.y);
 			// And then smoothing it out and applying it to the character
-			playerRigidbody2D.velocity = Vector3.SmoothDamp (playerRigidbody2D.velocity, targetVelocity, ref velocity, movementSmoothing);
-			
+			playerRigidbody2D.velocity = Vector3.SmoothDamp(playerRigidbody2D.velocity, targetVelocity, ref velocity, movementSmoothing);
+
 			CheckFlipBoundary();
 			Flip();
 		}
@@ -92,18 +89,18 @@ public class CharacterController2D : MonoBehaviour
 		if (grounded && jump) {
 			// Add a vertical force to the player.
 			grounded = false;
-			playerRigidbody2D.AddForce (new Vector2 (0f, jumpForce));
+			playerRigidbody2D.AddForce(new Vector2(0f, jumpForce));
 		}
 	}
 
-	private void Flip () {
+	private void Flip() {
 		// If the gun is facing to the right, flip character left
 		if (!facingRight) {
-			transform.rotation = Quaternion.Euler (new Vector3 (0, 180f, 0));
+			transform.rotation = Quaternion.Euler(new Vector3(0, 180f, 0));
 		}
 		// If the gun is facing to the right, flip character right
 		if (facingRight) {
-			transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 0));
+			transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
 		}
 	}
 
