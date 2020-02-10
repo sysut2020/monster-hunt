c1ee656a48a53f0a6169e52226a7e80a62f8a0f8
Trygve@spectre <blomsterpotte1998@gmail.com>
2020-02-10 15:19:37 +0100

    fixed the old system

--- unityfiles/Assets/Scripts/Game/Controls/AimControl.cs
+++ unityfiles/Assets/Scripts/Game/Controls/AimControl.cs
@@ -28,7 +28,7 @@ public class AimControl {
     private GameObject helperRotPoint;
     private GameObject helperAimPoint;
 
-    private bool debug = false;
+    private bool debug = true;
 
     public AimControl(GameObject firePoint, Transform rotatePoint) {
         this.FirePoint = firePoint;
@@ -70,21 +70,42 @@ public class AimControl {
         Vector3 v_RP_MP = mousePoint - RotationPoint.transform.position;
         float z_angle = this.RadianToDegree(Math.Atan2(v_RP_MP.y, v_RP_MP.x));
         bool isFlipped = (z_angle < 90 && z_angle > -90) ? false : true;
-        Vector3 angle = (isFlipped) ? new Vector3(0, 0, z_angle) : new Vector3(180, 0, -z_angle);
+        Vector3 angle = (isFlipped) ? new Vector3(180, 0, -z_angle): new Vector3(0, 0, z_angle);
         helperRotPoint.transform.rotation = Quaternion.Euler(angle);
+
+
         Vector3 v_GP_MP = mousePoint - helperAimPoint.transform.position;
         Vector3 norm_GP = helperAimPoint.transform.right;
         Vector3 norm_GP_Prj_GPMP = Vector3.Project(v_GP_MP, norm_GP);
-        Vector3 v_RP_GP_Prj_GPMP = (mousePoint + (norm_GP_Prj_GPMP - v_GP_MP)) - helperRotPoint.transform.position;
+
+        Vector3 norm_RPMP_Prj_GPMP = Vector3.Project(v_GP_MP, v_RP_MP.normalized);
+        Vector3 v_RP_GP_Prj_GPMP = (mousePoint - (norm_GP_Prj_GPMP - v_GP_MP)) - helperRotPoint.transform.position;
+        
         z_angle = this.RadianToDegree(Math.Atan2(v_RP_GP_Prj_GPMP.y, v_RP_GP_Prj_GPMP.x));
 
+        //z_angle = this.RadianToDegree(Math.Atan2(norm_GP_Prj_GPMP.y, norm_GP_Prj_GPMP.x));
+        //Debug.Log($"DEBUG ZANG {z_angle}");
+        //float additional = this.RadianToDegree(Math.Acos((new Vector2(v_GP_MP.x, v_GP_MP.y).magnitude/new Vector2(norm_GP_Prj_GPMP.x, norm_GP_Prj_GPMP.y).magnitude) % 1) );
+        //Debug.Log($"DEBUG additional {additional}");
+
+        // Debug.Log($"DEBUG Hosss {v_GP_MP.magnitude}");
+        // Debug.Log($"DEBUG A {v_GP_MP.x} {v_GP_MP.y} {v_GP_MP.z}");
+
+        // Debug.Log($"DEBUG mot {norm_GP_Prj_GPMP.magnitude}");
+        // Debug.Log($"DEBUG B {norm_GP_Prj_GPMP.x} {norm_GP_Prj_GPMP.y} {norm_GP_Prj_GPMP.z}");
+        
+
+        Debug.Log($"DEBUG ZANG2 {z_angle}");
+
         if (debug) {
-            Debug.DrawLine(Vector3.zero, norm_GP, Color.red);
+            Debug.DrawLine(Vector3.zero, norm_GP_Prj_GPMP, Color.red);
             Debug.DrawLine(Vector3.zero, v_GP_MP, Color.green);
-            Debug.DrawLine(Vector3.zero, Vector3.Project(v_GP_MP, norm_GP), Color.yellow);
-            Debug.DrawLine(Vector3.zero, norm_GP_Prj_GPMP - v_GP_MP, Color.blue);
+            // Debug.DrawLine(Vector3.zero, Vector3.Project(v_GP_MP, norm_GP), Color.yellow);
+            // Debug.DrawLine(mousePoint,mousePoint + (norm_GP_Prj_GPMP - v_GP_MP), Color.blue);
             Debug.DrawLine(RotationPoint.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), Color.magenta);
+
             Debug.DrawRay(FirePoint.transform.position, FirePoint.transform.right * 100, Color.cyan);
+            //Debug.DrawRay(FirePoint.transform.position, helperAimPoint.transform.right * 100, Color.white);
         }
 
         return (isFlipped) ? new Vector3(180, 0, -z_angle) : new Vector3(0, 0, z_angle);
