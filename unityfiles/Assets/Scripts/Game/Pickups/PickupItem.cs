a775605f3f62e6ce713a24f413edebcbbf5f8e5c
Emil <emil.nilsen9888@gmail.com>
2020-02-11 10:22:10 +0100

    Added collision detection

---
+++ unityfiles/Assets/Scripts/Game/Pickups/PickupItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour {
+
+    private PlayerInventory playerInventory;
+    
+    public void OnCollisionEnter2D(Collision2D collision) {
+
+        if (collision.gameObject.TryGetComponent(out PickupItem pickupItem)) {
+            Debug.Log("We hit the desired object");
+            
+        }
+
+    }
+}
