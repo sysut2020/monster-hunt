ddeee1ae4611e183ec01e7f0da8d8ba46a17d7fc
Steffen Holanger <holanger.steffen@gmail.com>
2020-02-11 13:23:18 +0100

    Formated code

--- unityfiles/Assets/Scripts/Game/Player/Player.cs
+++ unityfiles/Assets/Scripts/Game/Player/Player.cs
@@ -78,18 +78,19 @@ public class Player : MonoBehaviour {
     /// Gives a pickup to the player
     /// </summary>
     /// <param name="pickup">the pickup to give to the player</param>
-    public void GivePickup (IPickup pickup) {
+    public void GivePickup(IPickup pickup) {
         // activate
-        pickup.ApplyEffect (this);
+        pickup.ApplyEffect(this);
         // if effect is persistent give it to the watcher
         if (pickup is IEffectPickup) {
-            IEffectPickup equalTypeEffect = playerInventory.ActivePickups.Find (effect => effect.GetPickupName ().Equals (pickup.GetPickupName ()));
+            IEffectPickup equalTypeEffect =
+                playerInventory.ActivePickups.Find(effect => effect.GetPickupName().Equals(pickup.GetPickupName()));
             if (equalTypeEffect != null) {
                 // if it exists extend the existing
-                equalTypeEffect.ExtendEffect (equalTypeEffect);
+                equalTypeEffect.ExtendEffect(equalTypeEffect);
             } else {
                 // else just bop it on the end
-                this.PlayerInventory.AddEffectPickup (pickup as IEffectPickup);
+                this.PlayerInventory.AddEffectPickup(pickup as IEffectPickup);
             }
         }
     }
@@ -98,25 +99,25 @@ public class Player : MonoBehaviour {
     /// Gives money to the player
     /// </summary>
     /// <param name="money">the amount of money to give the player</param>
-    public void GiveMoney (int money) {
-        this.PlayerInventory.AddMoney (money);
+    public void GiveMoney(int money) {
+        this.PlayerInventory.AddMoney(money);
     }
 
     /// <summary>
     /// Gives a letter to the player
     /// </summary>
     /// <param name="letter">the letter to give to the player</param>        
-    public void GiveLetter (String letter) {
-        this.playerInventory.AddLetter (letter);
+    public void GiveLetter(String letter) {
+        this.playerInventory.AddLetter(letter);
     }
 
-    
+
     /// <summary>
     /// changes to the next weapon in the weapon list 
     /// if the end of the list is reached the index loops around
     /// </summary>
     /// <returns>The weapon controller of the new active weapon</returns>
-    public void ChangeToNextWeapon(){
+    public void ChangeToNextWeapon() {
         GunController newGunController = this.PlayerWeaponController.ChangeToNextWeapon();
         this.AlertPowerupsOnWeaponChange(newGunController);
     }
@@ -127,7 +128,7 @@ public class Player : MonoBehaviour {
     /// if the end of the list is reached the index loops around
     /// </summary>
     /// <returns>The weapon controller of the new active weapon</returns>
-    public void ChangeToPrevWeapon(){
+    public void ChangeToPrevWeapon() {
         GunController newGunController = this.PlayerWeaponController.ChangeToPrevWeapon();
         this.AlertPowerupsOnWeaponChange(newGunController);
     }
@@ -135,17 +136,23 @@ public class Player : MonoBehaviour {
     /// <summary>
     /// Starts to fire the weapon
     /// </summary>
-    public void StartFiring() {this.PlayerWeaponController.StartFiring();}
+    public void StartFiring() {
+        this.PlayerWeaponController.StartFiring();
+    }
 
     /// <summary>
     /// Stop fireing the weapon
     /// </summary>
-    public void StopFiring() {this.PlayerWeaponController.StopFiring();}
+    public void StopFiring() {
+        this.PlayerWeaponController.StopFiring();
+    }
 
     /// <summary>
     /// Fires a bullet if able (not on cooldown since last shot)
     /// </summary>
-    public void FireOnce() {this.PlayerWeaponController.FireOnce();}
+    public void FireOnce() {
+        this.PlayerWeaponController.FireOnce();
+    }
 
     // -- private -- // 
 
@@ -154,9 +161,9 @@ public class Player : MonoBehaviour {
     /// Alerts the power ups that the weapon has changed
     /// </summary>
     /// <param name="newGunC">The new gun controller to pass to the weapon</param>
-    private void AlertPowerupsOnWeaponChange(GunController newGunC){
-        foreach (IEffectPickup effect in this.PlayerInventory.ActivePickups.Reverse<IEffectPickup> ()) {
-            if (effect is IWeaponEffectPickup ) {
+    private void AlertPowerupsOnWeaponChange(GunController newGunC) {
+        foreach (IEffectPickup effect in this.PlayerInventory.ActivePickups.Reverse<IEffectPickup>()) {
+            if (effect is IWeaponEffectPickup) {
                 IWeaponEffectPickup weaponEffect = effect as IWeaponEffectPickup;
                 weaponEffect.OnChangeWeapon(newGunC);
             }
@@ -164,16 +171,15 @@ public class Player : MonoBehaviour {
     }
 
 
-
     /// <summary>
     /// iterates through the active effects and checks if any one of them are done
     /// if they are the effect is cleaned out and removed
     /// </summary>
-    private void UpdateEffects () {
-        foreach (IEffectPickup effect in this.PlayerInventory.ActivePickups.Reverse<IEffectPickup> ()) {
-            if (effect.IsEffectFinished ()) {
-                effect.Cleanup ();
-                this.playerInventory.RemoveEffectPickup (effect);
+    private void UpdateEffects() {
+        foreach (IEffectPickup effect in this.PlayerInventory.ActivePickups.Reverse<IEffectPickup>()) {
+            if (effect.IsEffectFinished()) {
+                effect.Cleanup();
+                this.playerInventory.RemoveEffectPickup(effect);
             }
         }
     }
