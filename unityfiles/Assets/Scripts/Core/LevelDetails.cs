75d1e19dab42b5fa5084221f1c890ac5980208b7
Steffen Holanger <holanger.steffen@gmail.com>
2020-02-11 16:02:11 +0100

    Commented class

--- unityfiles/Assets/Scripts/Core/LevelDetails.cs
+++ unityfiles/Assets/Scripts/Core/LevelDetails.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+
+/// <summary>
+/// Keeps data about a level
+/// </summary>
 [CreateAssetMenu(fileName = "Level Details", menuName = "Level details")]
 public class LevelDetails : ScriptableObject {
     public int time; // number of seconds the level last
