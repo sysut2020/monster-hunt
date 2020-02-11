02ba8305c36946095d0ee40ccece5117e8a4e858
Steffen Holanger <holanger.steffen@gmail.com>
2020-02-11 12:45:06 +0100

    Added listner for OnPlayerDead and ShowGameOver

--- unityfiles/Assets/Scripts/Core/GameManager.cs
+++ unityfiles/Assets/Scripts/Core/GameManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
     private static GameManager instance;
 
+    [SerializeField]
+    private GameObject gameOverCanvas;
     public static GameManager Instance {
         get {
             if (instance == null) {
@@ -22,7 +24,25 @@ public class GameManager : MonoBehaviour {
             DontDestroyOnLoad(gameObject);
         }
     }
-    
+
+    private void OnEnable() {
+        // todo subscribe to OnPlayerDead, OnTimeOut, OnAllEnemiesDead
+        Player.OnPlayerDead += ShowGameOver;
+    }
+
+    private void OnDestroy() {
+        // todo unsubscribe from OnPlayerDead, OnTimeOut, OnAllEnemiesDead
+        // maybe that this also should be done on disable
+        Player.OnPlayerDead -= ShowGameOver;
+    }
+
+    public void ShowGameOver() {
+        // todo show game over
+        gameOverCanvas.SetActive(true);   
+        Debug.Log("Player died");
+    }
+
+
     //-- Events --//
 
     public delegate void OnEndGame();
