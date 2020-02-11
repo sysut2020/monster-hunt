0c746bc0563744ba4f2a7a8c352281aea8ee1a17
Steffen Holanger <holanger.steffen@gmail.com>
2020-02-11 12:42:51 +0100

    Change singleton pattern to be GameObject

--- unityfiles/Assets/Scripts/Core/GameManager.cs
+++ unityfiles/Assets/Scripts/Core/GameManager.cs
@@ -1,16 +1,27 @@
-public class GameManager {
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
     private static GameManager instance;
 
     public static GameManager Instance {
         get {
             if (instance == null) {
-                instance = new GameManager();
+                GameObject gameObject = new GameObject("GameManager");
+                gameObject.AddComponent<GameManager>();
             }
 
             return instance;
         }
     }
     
+    private void Awake() {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+        }  else {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
     
     //-- Events --//
 
