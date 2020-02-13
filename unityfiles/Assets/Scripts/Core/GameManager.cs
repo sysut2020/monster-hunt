9695e9d72f056af3fd1ca79f3ad0e1344e6e5b6d
trygve@stasjoner <blomsterpotte1998@gmail.com>
2020-02-13 09:47:56 +0100

    Changed game manager to a game manager not a level manager

--- unityfiles/Assets/Scripts/Core/GameManager.cs
+++ unityfiles/Assets/Scripts/Core/GameManager.cs
@@ -2,18 +2,29 @@ using System;
 using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
 
-public class GameManager : MonoBehaviour {
-    private const int MAIN_MENU_SCENE_INDEX = 0;
-    
-    private static GameManager instance;
 
-    [SerializeField]
-    private GameObject gameOverCanvas;
+
+
+
+/// <summary>
+/// The manager for the whole game main task is to start and stop scenes and levels
+/// </summary>
+public class GameManager : MonoBehaviour {
+
+    
 
     [SerializeField]
     private LevelDetails levelDetails;
 
+
+
+    private const int MAIN_MENU_SCENE_INDEX = 0;
+    
+    private static GameManager instance;
+
     private int numberOfEnemies = 0;
+
+    // -- properties -- //
 
     /// <summary>
     /// Setting up singleton pattern
@@ -28,6 +39,40 @@ public class GameManager : MonoBehaviour {
             return instance;
         }
     }
+
+    // -- public -- //
+
+    /// <summary>
+    /// Decrements the number of enemies by one and then checks if there is any left
+    /// If there are none left it will fire the OnEndGame event, and then show the MainMenu
+    /// </summary>
+    public void DecrementEnemies() {
+        numberOfEnemies--;
+        if (numberOfEnemies <= 0) {
+            OnEndGame?.Invoke();
+            ShowMainMenu();
+        }
+    }
+
+    // -- events -- //    
+    private void SubscribeToEvents() {
+        // todo subscribe to OnPlayerDead, OnTimeOut, OnAllEnemiesDead
+        Enemy.OnEnemyDead += DecrementEnemies;
+        Player.OnPlayerDead += ShowGameOver;        
+    }
+
+    
+
+    private void UnsubscribeFromEvents() {
+        // todo unsubscribe from OnPlayerDead, OnTimeOut, OnAllEnemiesDead
+        // maybe that this also should be done on disable
+        Enemy.OnEnemyDead -= DecrementEnemies;
+        Player.OnPlayerDead -= ShowGameOver;
+    }
+
+    
+
+    // -- private -- //
 
     private void Awake() {
         if (instance != null && instance != this) {
@@ -51,43 +96,26 @@ public class GameManager : MonoBehaviour {
         UnsubscribeFromEvents();
     }
 
-    private void SubscribeToEvents() {
-        // todo subscribe to OnPlayerDead, OnTimeOut, OnAllEnemiesDead
-        Enemy.OnEnemyDead += DecrementEnemies;
-        Player.OnPlayerDead += ShowGameOver;
-    }
+    
 
-    /// <summary>
-    /// Decrements the number of enemies by one and then checks if there is any left
-    /// If there are none left it will fire the OnEndGame event, and then show the MainMenu
-    /// </summary>
-    public void DecrementEnemies() {
-        numberOfEnemies--;
-        if (numberOfEnemies <= 0) {
-            OnEndGame?.Invoke();
-            ShowMainMenu();
+    private void GameStateChange(String NewState){
+        switch (NewState)
+        {
+            case "MAIN_MENU":
+                Debug.Log($"New state : MAIN_MENU");
+                UnityEngine.SceneManagement.SceneManager.LoadScene(MAIN_MENU_SCENE_INDEX);
+                break;
+
+            default:
+                Debug.Log("UNKNOWN GAME STATE");
+                break;
         }
     }
 
-    private void UnsubscribeFromEvents() {
-        // todo unsubscribe from OnPlayerDead, OnTimeOut, OnAllEnemiesDead
-        // maybe that this also should be done on disable
-        Enemy.OnEnemyDead -= DecrementEnemies;
-        Player.OnPlayerDead -= ShowGameOver;
-    }
 
-    private void ShowGameOver() {
-        gameOverCanvas.SetActive(true);
-    }
 
-    private void ShowMainMenu() {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(MAIN_MENU_SCENE_INDEX);
-    }
 
 
     //-- Events --//
 
-    public delegate void EndGameHandler();
-
-    public static event EndGameHandler OnEndGame;
 }
