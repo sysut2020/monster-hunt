c38e48b6920d29bd75a81fc44905152029c6b485
Christoffer A Træen <christoffer.andersen@live.com>
2020-02-13 23:13:30 +0100

    Update inventory class to match events

--- unityfiles/Assets/Scripts/Game/Player/PlayerInventory.cs
+++ unityfiles/Assets/Scripts/Game/Player/PlayerInventory.cs
@@ -32,15 +32,15 @@ public class PlayerInventory : MonoBehaviour {
     }
 
     private void Start() {
-        CollectibleEvents.onCoinPickup += AddMoney;
-        CollectibleEvents.onLetterPickup += AddLetter;
-        CollectibleEvents.onPowerupPickup += AddEffectPickup;
+        CollectableEvents.OnCoinCollected += AddMoney;
+        CollectableEvents.OnLetterCollected += AddLetter;
+        CollectableEvents.OnPowerupCollected += AddEffectPickup;
     }
 
     private void OnDestroy() {
-        CollectibleEvents.onCoinPickup -= AddMoney;
-        CollectibleEvents.onLetterPickup -= AddLetter;
-        CollectibleEvents.onPowerupPickup -= AddEffectPickup;
+        CollectableEvents.OnCoinCollected -= AddMoney;
+        CollectableEvents.OnLetterCollected -= AddLetter;
+        CollectableEvents.OnPowerupCollected -= AddEffectPickup;
     }
     // -- public -- //
 
@@ -48,32 +48,32 @@ public class PlayerInventory : MonoBehaviour {
     /// Adds the given effect pickup
     /// </summary>
     /// <param name="effect">the pickup to add</param>
-    public void AddEffectPickup (IEffectPickup effect) {
-        this.activePickups.Add (effect);
+    public void AddEffectPickup(object sender, PowerUpCollectedArgs effect) {
+        this.activePickups.Add(effect.Effect);
     }
 
     /// <summary>
     /// removes the the given effect pickup
     /// </summary>
     /// <param name="effect">the pickup to remove</param>
-    public void RemoveEffectPickup (IEffectPickup effect) {
-        this.activePickups.Remove (effect);
+    public void RemoveEffectPickup(IEffectPickup effect) {
+        this.activePickups.Remove(effect);
     }
 
     /// <summary>
     /// adds the provided letter
     /// </summary>
     /// <param name="letter">the letter to add</param>
-    public void AddLetter (string letter) {
-        this.collectedLetters.Add(letter);
+    public void AddLetter(object sender, LetterCollectedArgs letter) {
+        this.collectedLetters.Add(letter.Letter);
     }
 
     /// <summary>
     /// adds the provided amount of money
     /// </summary>
     /// <param name="toAdd">the amount of money to add</param>
-    public void AddMoney (int value) {
-        this.money += value;
+    public void AddMoney(object sender, CoinCollectedArgs coin) {
+        this.money += coin.Amount;
     }
 
     // -- private -- // 
