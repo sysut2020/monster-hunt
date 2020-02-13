dc54880ca3966559b7fd7ff8461b701efa44d340
trygve@stasjoner <blomsterpotte1998@gmail.com>
2020-02-13 18:19:38 +0100

    added spawn point to level data

--- unityfiles/Assets/Scripts/Core/LevelDetails.cs
+++ unityfiles/Assets/Scripts/Core/LevelDetails.cs
@@ -7,13 +7,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Level Details", menuName = "Level details")]
 public class LevelDetails : ScriptableObject {
     [SerializeField]
-    private int time; // number of seconds the level should last
+    private int time;               // number of seconds the level should last
 
     [SerializeField]
-    private int numberOfEnemies;
+    private int numberOfEnemies;    // number of enemies to killed before level complete
 
     [SerializeField]
-    private int numberOfLetters; // number of letters to be spawned during game play
+    private int numberOfLetters;    // number of letters to be spawned during game play
+
+    public Vector2 spawnPoint;
 
     public int Time => time;
 
