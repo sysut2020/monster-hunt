de94ba93496e82cc881ba2d16479f467c97632ec
Christoffer A Træen <christoffer.andersen@live.com>
2020-02-17 00:20:26 +0100

    Update coin collect trigger method

--- unityfiles/Assets/Scripts/Game/Collectables/Coin.cs
+++ unityfiles/Assets/Scripts/Game/Collectables/Coin.cs
@@ -11,8 +11,10 @@ public class Coin : MonoBehaviour {
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("Player")){
-            CollectibleEvents.InvokeCoinPickup(10);
+        if (collision.gameObject.CompareTag("Player")) {
+            var args = new CoinCollectedArgs();
+            args.Amount = this.coinValue;
+            CollectableEvents.OnCoinCollected?.Invoke(this, args);
         }
     }
 }
