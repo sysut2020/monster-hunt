33da5113612b625607cd88c9bcef01d29c5093bc
Christoffer A Træen <christoffer.andersen@live.com>
2020-02-17 16:22:16 +0100

    WIP health and lives script

---
+++ unityfiles/Assets/Scripts/GUI/HealthbarGUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// This class is responsible for updating the health bar, displaying
+/// amount of health as a pool of health, which increase / decrease as health
+/// changes.
+/// </summary>
+public class HealthbarGUI : MonoBehaviour {
+
+    /// <summary>
+    /// Maximum health so we know what is maximum
+    /// </summary>
+    private float maxHealth = 0;
+
+    /// <summary>
+    /// Current health value
+    /// </summary>
+    private float currentHealth = 0;
+
+    public float CurrentHealth {
+        get { return this.currentHealth; }
+        set { this.currentHealth = value; }
+    }
+
+    private void Awake() {
+        // if (coinCounter == null) throw new MissingComponentException("Missing text component");
+        // CollectableEvents.OnCoinCollected += OnHealthChanged;
+    }
+
+    private void UpdateHealthPool() {
+
+    }
+
+    private void OnHealthChanged(object sender, int healthChange) {
+        this.CurrentHealth = healthChange;
+        this.UpdateHealthPool();
+    }
+
+    private void OnDestroy() {
+        // CollectableEvents.OnCoinCollected -= OnHealthChanged;
+    }
+}
---
+++ unityfiles/Assets/Scripts/GUI/LivesGUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesGUI : MonoBehaviour {
+
+    private int maxLives = 3;
+
+    private int currentLives = 3;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
