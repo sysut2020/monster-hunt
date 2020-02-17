0018a77b97230e1292b02f2dac971e71bd4b4439
trygve@stasjoner <blomsterpotte1998@gmail.com>
2020-02-17 22:39:55 +0100

    cleanup of scriptable objects

--- unityfiles/Assets/Scripts/Core/LevelDetails.cs
+++ unityfiles/Assets/Scripts/Core/LevelDetails.cs
@@ -6,20 +6,20 @@ using UnityEngine;
 /// </summary>
 [CreateAssetMenu(fileName = "Level Details", menuName = "Level details")]
 public class LevelDetails : ScriptableObject {
+
+    [Tooltip ("number of seconds the level should last")]
     [SerializeField]
-    private int time;               // number of seconds the level should last
+    private int time;
 
+    [Tooltip ("number of enemies to killed before level complete")]
     [SerializeField]
-    private int numberOfEnemies;    // number of enemies to killed before level complete
+    private int numberOfEnemies;
 
+    [Tooltip ("number of letters to be spawned during game play")]
     [SerializeField]
-    private int numberOfLetters;    // number of letters to be spawned during game play
+    private int numberOfLetters;
 
-    public Vector2 spawnPoint;
-
-    public int Time => time;
-
-    public int NumberOfEnemies => numberOfEnemies;
-
-    public int NumberOfLetters => numberOfLetters;
+    public int Time { get => time;}
+    public int NumberOfEnemies { get => numberOfEnemies;}
+    public int NumberOfLetters { get => numberOfLetters;}
 }
