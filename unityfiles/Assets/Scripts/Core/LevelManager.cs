1473e0b207a3c343f2560edc7a2dbd37bd8faca9
Christoffer A Træen <christoffer.andersen@live.com>
2020-02-18 21:18:37 +0100

    Fix spawning on level start

--- unityfiles/Assets/Scripts/Core/LevelManager.cs
+++ unityfiles/Assets/Scripts/Core/LevelManager.cs
@@ -1,21 +1,19 @@
 using System;
 using UnityEngine;
 
-
 /// <summary>
 /// The event data for the game state changed events 
 /// </summary>
-public class LevelStateChangeEventArgs: EventArgs{
-    public LEVEL_STATE NewState {get; set;}
+public class LevelStateChangeEventArgs : EventArgs {
+    public LEVEL_STATE NewState { get; set; }
 }
 
 /// <summary>
 /// The temporary data needed while playing through the level
 /// </summary>
-class PlayThroughData{
-    public int EnemysKilled {get; set;}
+class PlayThroughData {
+    public int EnemysKilled { get; set; }
 }
-
 
 /// <summary>
 /// A manager for a level in the game 
@@ -24,26 +22,20 @@ public class LevelManager : Singleton<LevelManager> {
     [SerializeField]
     private GameObject gameOverCanvas;
 
-
     [SerializeField]
     private LevelDetails levelDetails;
-
 
     private Timers levelTimer = new Timers();
     private string LEVEL_TIMER_ID;
     private LEVEL_STATE currentState; // may need default here in that case find out the starting state
 
     private PlayThroughData playThroughData;
-    
-
-
 
     // -- properties -- //
 
     // -- public -- //
 
     // -- events -- //
-
 
     /// <summary>
     /// This event tells the listeners the level state has changed
@@ -59,21 +51,21 @@ public class LevelManager : Singleton<LevelManager> {
     /// <summary>
     /// Subscribes to the relevant events for this class
     /// </summary>
-    private void SubscribeToEvents(){
-        CleanUpEvent             += (object __, EventArgs _) => this.UnsubscribeFromEvents();
+    private void SubscribeToEvents() {
+        CleanUpEvent += (object __, EventArgs _) => this.UnsubscribeFromEvents();
 
         Player.PlayerKilledEvent += c_PlayerKilledEvent;
-        Enemy.EnemyKilledEvent   += c_EnemyKilledEvent;
+        Enemy.EnemyKilledEvent += c_EnemyKilledEvent;
     }
 
     /// <summary>
     /// Subscribes to the relevant events for this class
     /// </summary>
-    private void UnsubscribeFromEvents(){
+    private void UnsubscribeFromEvents() {
         CleanUpEvent -= (object __, EventArgs _) => this.UnsubscribeFromEvents();;
 
         Player.PlayerKilledEvent -= c_PlayerKilledEvent;
-        Enemy.EnemyKilledEvent   -= c_EnemyKilledEvent;
+        Enemy.EnemyKilledEvent -= c_EnemyKilledEvent;
     }
 
     /// <summary>
@@ -82,7 +74,7 @@ public class LevelManager : Singleton<LevelManager> {
     /// </summary>
     /// <param name="o">the object calling</param>
     /// <param name="args">the event args</param>
-    private void c_PlayerKilledEvent(object o, EventArgs _){
+    private void c_PlayerKilledEvent(object o, EventArgs _) {
         LevelStateChange(LEVEL_STATE.GAME_OVER);
     }
 
@@ -92,38 +84,35 @@ public class LevelManager : Singleton<LevelManager> {
     /// </summary>
     /// <param name="o">the object calling</param>
     /// <param name="args">the event args</param>
-    private void c_EnemyKilledEvent(object o, EnemyEventArgs args){
+    private void c_EnemyKilledEvent(object o, EnemyEventArgs args) {
         playThroughData.EnemysKilled += 1;
-        if (this.levelDetails.NumberOfEnemies <= playThroughData.EnemysKilled){
+        if (this.levelDetails.NumberOfEnemies <= playThroughData.EnemysKilled) {
             this.LevelStateChange(LEVEL_STATE.EXIT);
         }
     }
-
- 
 
     /// <summary>
     /// Changes the level state 
     /// </summary>
     /// <param name="NewState">The new level state</param>
-    private void LevelStateChange(LEVEL_STATE NewState){
+    private void LevelStateChange(LEVEL_STATE NewState) {
 
         this.currentState = NewState;
         LevelStateChangeEventArgs args = new LevelStateChangeEventArgs();
         args.NewState = NewState;
-        
-        
-        switch (NewState)
-        {
+
+        switch (NewState) {
             /// The game is over show game over screen
             case LEVEL_STATE.GAME_OVER:
                 gameOverCanvas.SetActive(true);
                 break;
 
-            /// Start the main mode spawn the player and start the level
-            case LEVEL_STATE.HUNTING:                
+                /// Start the main mode spawn the player and start the level
+            case LEVEL_STATE.HUNTING:
+                InitLevel();
                 break;
 
-            /// Exit the game and go to main menu
+                /// Exit the game and go to main menu
             case LEVEL_STATE.EXIT:
                 break;
 
@@ -135,49 +124,41 @@ public class LevelManager : Singleton<LevelManager> {
         LevelStateChangeEvent?.Invoke(this, args);
     }
 
-
     // -- private -- //
-
 
     // TODO: maybe remove?
     /// <summary>
     /// spawns all the enemys and inits the player
     /// </summary>
-    private void InitLevel(){
+    private void InitLevel() {
         Spawner.Instance?.SpawnOnAll();
     }
-
-    
-
 
     /// <summary>
     /// Triggers an event telling the listeners they are about to be deleted and
     /// and should unsubscribe from all events osv
     /// </summary>
-    private void CleanUpScene(){
+    private void CleanUpScene() {
         CleanUpEvent?.Invoke(this, EventArgs.Empty);
     }
 
-    private void startLevelTime(){
+    private void startLevelTime() {
         this.levelTimer.Set(LEVEL_TIMER_ID, this.levelDetails.Time);
-        
+
     }
-
-
-
 
     // -- unity -- //
 
-    private void Start(){
+    private void Start() {
         this.playThroughData = new PlayThroughData();
         LEVEL_TIMER_ID = this.levelTimer.RollingUID;
         this.startLevelTime();
 
         this.LevelStateChange(LEVEL_STATE.HUNTING);
     }
-    private void Update(){
+    private void Update() {
 
-        if (this.levelTimer.Done(LEVEL_TIMER_ID)){
+        if (this.levelTimer.Done(LEVEL_TIMER_ID)) {
             this.LevelStateChange(LEVEL_STATE.EXIT);
         }
     }
@@ -185,7 +166,7 @@ public class LevelManager : Singleton<LevelManager> {
     private void OnEnable() {
         SubscribeToEvents();
     }
-    
+
     private void OnDestroy() {
         UnsubscribeFromEvents();
     }
