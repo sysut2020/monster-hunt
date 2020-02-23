32a61c49db6f8749078d5604de7be28f478771c2
Trygve@spectre <blomsterpotte1998@gmail.com>
2020-02-23 20:47:07 +0100

    pl anim const

--- unityfiles/Assets/Scripts/Helper/consts.cs
+++ unityfiles/Assets/Scripts/Helper/consts.cs
@@ -42,3 +42,20 @@ public enum LEVEL_STATE {
     HUNTING
 }
 
+/// <summary>
+/// 
+/// </summary>
+public enum PLAYER_ANIMATION {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    HOLD_SNIPER = 1,
+    
+    /// <summary>
+    /// 
+    /// </summary>
+	HOLD_RAYGUN = 2,
+
+}
+
