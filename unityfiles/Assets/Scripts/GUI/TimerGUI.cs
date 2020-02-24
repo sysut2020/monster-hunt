bff967de42164c9e5ca9363195c6c93070fd76cb
Emil <emil.nilsen9888@gmail.com>
2020-02-24 15:43:57 +0100

    Add timer work

--- unityfiles/Assets/Scripts/GUI/TimerGUI.cs
+++ unityfiles/Assets/Scripts/GUI/TimerGUI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -6,8 +7,27 @@ using UnityEngine;
 public class TimerGUI : MonoBehaviour {
 
     [SerializeField] 
-    private TMP_Text timer;
+    private TMP_Text timerText;
     
-    
-    
+    [SerializeField]
+    private float levelTime = 1000;
+
+    private string timerID = "1";
+    private Timers timer;
+
+    private void Awake() {
+        if (timerText == null){
+            throw new MissingComponentException("Missing text component");
+        }
+        this.timer = new Timers();
+        timer.Set(timerID, levelTime);
+    }
+
+    private void Update() {
+        SetTimerText();
+    }
+
+    private void SetTimerText() {
+        this.timerText.SetText($"{timer.TimeLeft(timerID)}");
+    }
 }
