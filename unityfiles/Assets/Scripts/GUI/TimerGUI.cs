3c8b820d63e178cbf38092b2a33e37d517f1e2c7
Emil <emil.nilsen9888@gmail.com>
2020-02-24 15:06:57 +0100

    Started work on timer GUI

---
+++ unityfiles/Assets/Scripts/GUI/TimerGUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimerGUI : MonoBehaviour {
+
+    [SerializeField] 
+    private TMP_Text timer;
+    
+    
+    
+}
