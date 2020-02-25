4aeb2e34cd8e4526407ea318eebf630db3c6768b
Steffen Holanger <holanger.steffen@gmail.com>
2020-02-25 12:52:07 +0100

    Pausemenu prefab and script

---
+++ unityfiles/Assets/Scripts/Game/Menu/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+    [SerializeField] private GameObject pauseMenuCanvas;
+
+    private Boolean isPaused = false;
+
+    private Button continueButton;
+    private static readonly int PAUSE = 0;
+    private static readonly float PLAY = 1;
+
+    private void Awake() {
+        DeactivateMenu();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Cancel")) {
+            isPaused = !isPaused;
+
+            if (isPaused) {
+                ActivateMenu();
+            } else {
+                DeactivateMenu();
+            }
+        }
+    }
+
+    void ActivateMenu() {
+        Time.timeScale = PAUSE;
+        //Time.fixedDeltaTime = PAUSE;
+        pauseMenuCanvas.SetActive(true);
+    }
+
+    public void DeactivateMenu() {
+        Time.timeScale = PLAY;
+        pauseMenuCanvas.SetActive(false);
+        isPaused = false;
+    }
+}
