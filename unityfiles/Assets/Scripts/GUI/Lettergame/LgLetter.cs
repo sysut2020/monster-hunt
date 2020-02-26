fb6c5017fc092073c4d7a5d5c79db1adb3fa1151
trygve@stasjoner <trygve.woldseth@gmail.com>
2020-02-26 19:07:51 +0100

    made data class for the in game letter

---
+++ unityfiles/Assets/Scripts/GUI/Lettergame/LgLetter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class LgLetter{
+
+    private readonly string letter;
+    private int xPos, yPos;
+    private bool isOnBoard = false;
+
+    public LgLetter(int x, int y, string tileLetter){
+        this.XPos = x;
+        this.YPos = y;
+        this.letter = tileLetter;
+    }
+
+    public string Letter { get => letter;}
+    public bool IsOnBoard { get => isOnBoard; set => isOnBoard = value; }
+    public int YPos { get => yPos; set => yPos = value; }
+    public int XPos { get => xPos; set => xPos = value; }
+}
