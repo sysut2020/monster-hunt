9e0403fc53e2999849131a04b027d4658ae03ea3
Christoffer A Træen <christoffer.andersen@live.com>
2020-02-27 12:02:38 +0100

    Fix binary search bug

--- unityfiles/Assets/Scripts/GUI/Lettergame/WordCheker.cs
+++ unityfiles/Assets/Scripts/GUI/Lettergame/WordCheker.cs
@@ -1,22 +1,17 @@
-using System.Linq;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-
-public class WordChecker
-{
+public class WordChecker {
 
     //REMOVE AND READ FROM FILE
-    private static string[] words = {"AAB", "ABA", "BOB", "HELLO", "UNCLE"};
+    private static String[] words = { "AAB", "ABA", "BOB", "HELLO", "UNCLE" };
     //REMOVE AND READ FROM FILE
-
-    
 
     // -- private -- // 
 
-    public static bool isWordValid(string w){
-        return Array.BinarySearch(words, w) > 0;
+    public static bool isWordValid(string w) {
+        // Have to sort the array, event if it is meade sorted
+        Array.Sort(words);
+        // Returns the index in the array where it found the word. Negetive value if nothing was found
+        return Array.BinarySearch(words, w) >= 0;
     }
 }
