7fa19ce186883dc17bdf2fc9c76b4a830f1fb69e
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-02 15:24:51 +0100

    Enemy health bar gui controller init

---
+++ unityfiles/Assets/EnemyHealthBarGUIController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Takes care of displaying and updating the enemy's health bar
+/// </summary>
+public class EnemyHealthBarGUIController : MonoBehaviour {
+    
+    [SerializeField]
+    private GameObject healthBarBackground;
+
+    private float startHealth;
+
+    [SerializeField]
+    private Image healthBar;
+
+
+    public float StartHealth {
+        set => startHealth = value;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        HideHealthBar();
+    }
+
+    private void HideHealthBar() {
+        healthBarBackground.SetActive(false);
+    }
+
+    private void DisplayHealthBar() {
+        healthBarBackground.SetActive(true);
+    }
+
+    /// <summary>
+    /// Updates the healthbar to display the remaining health as a fraction of the start health
+    /// </summary>
+    /// <param name="health">how much the health the enemy currently has</param>
+    public void UpdateHealthBar(float health) {
+        if (!healthBarBackground.activeSelf) { // if the health bar is not shown, show it.
+            DisplayHealthBar();
+        }
+        healthBar.fillAmount = health / startHealth;
+    }
+}
