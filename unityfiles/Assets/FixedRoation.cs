af040160573ddb89d7176614be4e73caa3842f3e
Christoffer A Træen <christoffer.andersen@live.com>
2020-03-02 22:44:38 +0100

    Add boolean to fixed rotation

--- unityfiles/Assets/FixedRoation.cs
+++ unityfiles/Assets/FixedRoation.cs
@@ -6,15 +6,17 @@ using UnityEngine;
 /// Fixes the roatation of the game object the script sits on
 /// Will prevent flipping from happening.
 /// </summary>
-public class FixedRoation : MonoBehaviour
-{
-    Quaternion rotation;
-    void Awake()
-    {
-        rotation = transform.rotation;
+public class FixedRoation : MonoBehaviour {
+
+    Quaternion initialRotation;
+
+    void Awake() {
+        this.initialRotation = this.transform.rotation;
     }
-    void LateUpdate()
-    {
-        transform.rotation = rotation;
+
+    void LateUpdate() {
+        if (!this.transform.rotation.Equals(initialRotation)) {
+            this.transform.rotation = initialRotation;
+        }
     }
 }
