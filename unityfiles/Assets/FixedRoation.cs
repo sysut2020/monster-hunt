a8fade671a25b7ac3b58f98cc538eb47d96e12cd
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-02 15:06:54 +0100

    Fixed rotation script

---
+++ unityfiles/Assets/FixedRoation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Fixes the roatation of the game object the script sits on
+/// Will prevent flipping from happening.
+/// </summary>
+public class FixedRoation : MonoBehaviour
+{
+    Quaternion rotation;
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+    void LateUpdate()
+    {
+        transform.rotation = rotation;
+    }
+}
