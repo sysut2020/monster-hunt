882460bf6cfec265af7314515c972873c534011d
Emil <emil.nilsen9888@gmail.com>
2020-03-10 10:43:52 +0100

    Add audio manager prefab

--- unityfiles/Assets/Scripts/Audio/AudioManager.cs
+++ unityfiles/Assets/Scripts/Audio/AudioManager.cs
@@ -1,7 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
+
+    private Sound[] sounds;
+
+    private static AudioManager instance;
     
+    private void Awake() {
+        
+        DontDestroyOnLoad(gameObject);
+        
+        foreach (Sound s in sounds) {
+            s.audioSource = gameObject.AddComponent<AudioSource>();
+            s.audioSource.clip = s.audioClip;
+
+            s.audioSource.volume = s.volume;
+            s.audioSource.pitch = s.pitch;
+            s.audioSource.loop = s.loop;
+        }
+    }
+
+    private void Start() {
+        Play("Level 1 music");
+    }
+
+    public void Play(string name) {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null) {
+            Debug.LogWarning("Sound: " + name +" not found!");
+            return;
+        }
+        s.audioSource.Play();
+    }
 }
--- unityfiles/Assets/Scripts/Audio/Sound.cs
+++ unityfiles/Assets/Scripts/Audio/Sound.cs
@@ -2,6 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Sound {
+
+    public string name;
     
+    public AudioClip audioClip;
+    
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0.1f, 3f)]
+    public float pitch;
+
+    public bool loop;
+    
+    [HideInInspector]
+    public AudioSource audioSource;
 }
