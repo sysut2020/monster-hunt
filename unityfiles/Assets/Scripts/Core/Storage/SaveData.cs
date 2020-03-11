f67be76b989355f1964d3993b174252af23699db
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-11 13:30:46 +0100

    Changed type of highscores

--- unityfiles/Assets/Scripts/Core/Storage/SaveData.cs
+++ unityfiles/Assets/Scripts/Core/Storage/SaveData.cs
@@ -1,15 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
+
 /// <summary>
 /// Simple class which is used in part with "DataHandler" class.
 /// All information we want to store, can be kept in this class
 /// </summary>
 [System.Serializable]
 public class SaveData {
-    
-    public Dictionary<string, int> HighScores; 
+    public List<ScoreboardEntry> HighScores;
     public int Money;
-
-
 }
