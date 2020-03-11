3c9d859fa505d15b9c409c155b187f3b845f02ea
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-11 13:36:23 +0100

    changed from dictonary to list

--- unityfiles/Assets/Scripts/Core/Storage/GameDataManager.cs
+++ unityfiles/Assets/Scripts/Core/Storage/GameDataManager.cs
@@ -12,14 +12,14 @@ public class PlayerDestroyedEventArgs : EventArgs {
 /// Used to get persistent information from the players inventory
 /// </summary>
 public class GameDataManager {
-    
-
     private Dictionary<string, int> playerLetters;
     private int money;
+    private List<ScoreboardEntry> highScores;
 
     public GameDataManager() {
         this.playerLetters = new Dictionary<string, int>();
         this.money = 0;
+        this.highScores = new List<ScoreboardEntry>();
 
         this.LoadData();
     }
@@ -29,60 +29,69 @@ public class GameDataManager {
     public Dictionary<string, int> PlayerLetters {
         get => playerLetters;
     }
-    
+
     public int Money {
         get => money;
     }
+
+    public List<ScoreboardEntry> HighScores {
+        get => highScores;
+    }
     // -- events -- // 
 
-    
+
     // -- public -- //
 
-    public void AddLetters(Dictionary<string, int> toAdd){
-        foreach (string key in toAdd.Keys){
-            playerLetters[key] =+ toAdd[key];
+    public void AddLetters(Dictionary<string, int> toAdd) {
+        foreach (string key in toAdd.Keys) {
+            playerLetters[key] = +toAdd[key];
         }
     }
 
-    public void SetLetters(Dictionary<string, int> l){
+    public void SetLetters(Dictionary<string, int> l) {
         this.playerLetters = l;
     }
 
-    public void AddMoney(int toAdd){
+    public void AddMoney(int toAdd) {
         this.money += toAdd;
     }
 
-    public void SetMoney(int m){
+    public void SetMoney(int m) {
         this.money = m;
     }
 
-    public void SaveData(){
+    private void AddNewHighScore(string name, int score) {
+        highScores.Add(new ScoreboardEntry(name, score));
+    }
+
+    public void SetHighScores(List<ScoreboardEntry> highScores) {
+        this.highScores = highScores;
+    }
+
+    public void SaveData() {
         SaveData saveObj = new SaveData();
         saveObj.Money = this.money;
-        // saveObj.HighScores = steffanos work your magic here
+        saveObj.HighScores = this.highScores;
         DataSaver.Save(saveObj);
     }
 
     // -- private -- // 
 
-    
 
-
-    private void LoadData(){
+    private void LoadData() {
         SaveData saveObj = DataSaver.Load();
-        if(saveObj != null) {
-            if(saveObj?.Money != 0) {this.AddMoney(saveObj.Money);}
-            Debug.Log(money);
-            //if(saveObj?.HighScores != 0) {steffanos work your magic here;}
+        if (saveObj != null) {
+            if (saveObj?.Money != 0) {
+                this.AddMoney(saveObj.Money);
+            }
+            
+            if (saveObj?.HighScores != null) {
+                SetHighScores(saveObj.HighScores);
+            }
+
+            // foreach (var entry in HighScores) {
+            //     Debug.Log("entry: " + entry.Name);
+            // }
         }
-        
-  
-        
     }
-
-    
-    
-    
-    
-
 }
