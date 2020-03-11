1a0b8b08fbd88b194f8d07ae58bea301e8259e85
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-11 15:02:35 +0100

    setting the score text in callbacks

--- unityfiles/Assets/Scripts/GUI/ScoreGUI.cs
+++ unityfiles/Assets/Scripts/GUI/ScoreGUI.cs
@@ -8,9 +8,9 @@ public class ScoreGUI : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        scoreText = GetComponent<TextMeshProUGUI>();
-        scoreText.text = totalLevelScore.ToString();
+        scoreText = gameObject.GetComponent<TextMeshProUGUI>();
 
+        UpdateScoreText();
         SubscribeToEvents();
     }
 
@@ -19,6 +19,12 @@ public class ScoreGUI : MonoBehaviour {
         // todo save score
     }
 
+    /// <summary>
+    /// Updates the score text to the total score
+    /// </summary>
+    private void UpdateScoreText() {
+        scoreText.text = totalLevelScore.ToString();
+    }
 
     private void SubscribeToEvents() {
         CoinCollectable.OnCoinCollected += CallbackCoinCollected;
@@ -34,17 +40,16 @@ public class ScoreGUI : MonoBehaviour {
 
     private void CallbackEffectPickup(object sender, PowerUpCollectedArgs e) {
         totalLevelScore++; // when a power up is picked up we add one point to the total score
+        UpdateScoreText();
     }
 
     private void CallbackLetterCollected(object __, LetterCollectedArgs _) {
         totalLevelScore++; // when a letter is collected we add one point to the total score counter
+        UpdateScoreText();
     }
 
     private void CallbackCoinCollected(object _, CoinCollectedArgs args) {
         totalLevelScore += args.Amount;
-    }
-
-    // Update is called once per frame
-    void Update() {
+        UpdateScoreText();
     }
 }
