d8fdc789a4ff0639b105ec71a2187dbf381ebc14
trygve@stasjoner <blomsterpotte1998@gmail.com>
2020-03-12 19:01:09 +0100

    updatet bullet controll

--- unityfiles/Assets/Scripts/Game/Weapon/BulletControl.cs
+++ unityfiles/Assets/Scripts/Game/Weapon/BulletControl.cs
@@ -9,84 +9,121 @@ TODO: maybe blop this in an interface like the HC/enemy relation
 
 public class BulletControl : MonoBehaviour {
 
-    // -- internal -- // 
-    [SerializeField]
-    private Vector2 velocity;
-    [SerializeField]
-    private float bulletTimeToLive;
-    [SerializeField]
-    private float damage;
+
+    private BulletData bulletData;
+    private BulletData currentBuildBulletData = null;
+    private IEnumerator activeEnumerator;
+
+
+
 
     // -- properties -- //
 
-    public Vector2 Velocity {
-        get { return this.velocity; }
-        set { this.velocity = value; }
-    }
-    public float BulletTimeToLive {
-        get { return this.bulletTimeToLive; }
-        set { this.bulletTimeToLive = value; }
+    internal BulletData BulletData {
+        get{
+            if (bulletData == null){
+                throw new System.Exception("Bullet data missing");
+            }
+            return bulletData;
+        }
+        
+        set{
+            this.bulletData = value;
+            this.BuildBullet();
+        }
     }
 
-    public float Damage {
-        get { return this.damage; }
-        set { this.damage = value; }
-    }
+    private void BuildBullet(){
+        if (this.currentBuildBulletData != this.bulletData){
+            
+            SpriteRenderer spriteRender = null;
+            BoxCollider2D boxCol = null;
+            Rigidbody2D rigidB2d = null;
 
-    private float killAt;
-    private bool isActive;
+            
+
+            
+
+            this.gameObject.transform.localScale = this.bulletData.SpriteTransform.lossyScale;
+      
+
+
+            if (!this.gameObject.TryGetComponent(out spriteRender)) {
+                spriteRender = this.gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
+            } 
+            spriteRender.sprite = this.bulletData.Sprite;
+
+
+            if (!this.gameObject.TryGetComponent(out rigidB2d)) {
+                rigidB2d = this.gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
+            } 
+            rigidB2d.bodyType = RigidbodyType2D.Kinematic;
+
+            if (!this.gameObject.TryGetComponent(out boxCol)) {
+                boxCol = this.gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
+            }
+            boxCol.isTrigger = true;
+            boxCol.size = spriteRender.sprite.bounds.size;
+            
+            
+            this.gameObject.SetActive(false);
+            currentBuildBulletData = bulletData;
+        }
+        
+    }
 
     /// <summary>
     /// Starts the time to live timer
-    /// 
     /// </summary>
-    public void StartTtlTimer() {
-        this.killAt = Time.time + this.bulletTimeToLive;
+    private void StartTtlTimer() {
+        this.activeEnumerator = TtlTimer(this.BulletData.TimeToLive);
+        StartCoroutine(this.activeEnumerator);
+    }
+
+
+    private IEnumerator TtlTimer(int waitTime){
+        yield return new WaitForSeconds(waitTime);
+        this.DisableSelf();      
     }
 
     /// <summary>
     /// Disables the game object so it can be 
     /// collected in the local gun controller
     /// </summary>
-    void KillSelf() {
+    private void DisableSelf() {
         this.gameObject.SetActive(false);
+        StopCoroutine(this.activeEnumerator);
     }
+
+    
 
     // -- unity -- //
-
-    void Start() {
-        this.StartTtlTimer(); // to avoid potential instant removal
-        this.tag = "Bullet";
-    }
-
     void Update() {
-        Vector2 localVelocity = new Vector2(
-            this.Velocity.x, this.Velocity.y
-        ) * Time.deltaTime;
+        Vector2 localVelocity = new Vector2(this.BulletData.Velocity, 0) * Time.deltaTime;
         this.gameObject.transform.Translate(localVelocity.x, localVelocity.y, 0);
-        if (Time.time > this.killAt && this.isActive) {
-            this.isActive = false;
-            this.KillSelf();
-        }
     }
 
     void OnTriggerEnter2D(Collider2D Col) {
-
         if (!Col.TryGetComponent(out PlayerHealthController ph)) {
             if (Col.TryGetComponent(out EnemyHealthController enemyHealth)) {
-                enemyHealth.ApplyDamage(this.damage);
+                enemyHealth.ApplyDamage(this.BulletData.Damage);
             }
         }
 
         if (!Col.TryGetComponent(out BulletControl _) && !Col.TryGetComponent(out PlayerHealthController a)){
-            this.KillSelf();
+            this.DisableSelf();
         }
         
     }
 
-    void OnEnable() {
-        this.isActive = true;
-        this.StartTtlTimer();
+    void OnDestroy() {
+        if (this.activeEnumerator != null){
+            StopCoroutine(this.activeEnumerator);
+        }
     }
+
+    void OnEnable() { 
+        this.StartTtlTimer();
+    } 
 
 }
