70ed24fe1e85f30efd8ff276754ac0134aa09575
Christoffer A Træen <christoffer.andersen@live.com>
2020-03-13 12:22:24 +0100

    Start file reader class

---
+++ unityfiles/Assets/Scripts/OS/FileReader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Monsterhunt.Fileoperation {
+    public class FileReader {
+
+        public class FileContent {
+
+            internal FileContent() { }
+            internal string[] Content { get; set; } = new string[0];
+            public List<string> AsList() {
+                return this.Content.ToList();
+            }
+
+            public string[] AsArray() {
+                return this.Content;
+            }
+        }
+
+        private string filePath;
+        private string FilePath {
+            get { return filePath; }
+            set {
+                if (value == null) {
+                    throw new NullReferenceException("Path to file is null");
+                } else {
+                    filePath = value;
+                }
+            }
+        }
+
+        public FileReader(string filePath) {
+            this.FilePath = filePath;
+        }
+
+        public FileContent ReadAllLines() {
+            var fc = new FileContent();
+            try {
+                fc.Content = File.ReadAllLines(this.filePath);
+            } catch (SystemException e) {
+                Debug.LogError(e.Message);
+            }
+            return fc;
+        }
+
+    }
+}
