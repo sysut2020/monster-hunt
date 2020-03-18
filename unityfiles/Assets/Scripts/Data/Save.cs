08c8bab2432d42b83f62e73f437d13523f87592a
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-18 12:23:31 +0100

    added score

--- unityfiles/Assets/Scripts/Data/Save.cs
+++ unityfiles/Assets/Scripts/Data/Save.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine.SocialPlatforms.Impl;
+
 /// <summary>
 /// Simple class which is used in part with "DataHandler" class.
 /// All information we want to store, can be kept in this class
@@ -10,5 +10,11 @@ public class Save {
     
     public int SavedMoney; 
     public Dictionary<string, int> availableLetters;
-    
+
+    private int score;
+
+    public int Score {
+        get => score;
+        set => score = value;
+    }
 }
