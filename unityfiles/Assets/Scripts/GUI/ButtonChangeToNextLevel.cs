b76a1db45eb89a39d20571762a4212dea69acb48
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-22 21:58:15 +0100

    Added button script

---
+++ unityfiles/Assets/Scripts/GUI/ButtonChangeToNextLevel.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Changes to next level
+/// </summary>
+[RequireComponent(typeof(Button))]
+public class ButtonChangeToNextLevel : ChangeEventButton {
+    private GAME_STATE eventToFire = GAME_STATE.NEXT_LEVEL;
+
+    public static event EventHandler<ButtonClickEventArgs> buttonEventHandler;
+    protected override Enum EventToFire => eventToFire;
+    protected override EventHandler<ButtonClickEventArgs> ButtonEventHandler => buttonEventHandler;
+}
