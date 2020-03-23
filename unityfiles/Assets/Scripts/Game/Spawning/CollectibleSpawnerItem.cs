1ba1459bc5c0fd181d4a4380971c7fc9b13fcf09
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-23 17:09:53 +0100

    added comment and tooltip

--- unityfiles/Assets/Scripts/Game/Spawning/CollectibleSpawnerItem.cs
+++ unityfiles/Assets/Scripts/Game/Spawning/CollectibleSpawnerItem.cs
@@ -12,8 +12,12 @@ public class CollectibleSpawnerItem {
 	private string itemName;
 	public string ItemName { get => itemName; private set => itemName = value; }
 
+	/// <summary>
+	/// Higher value higher chance of been spawned
+	/// </summary>
 	[SerializeField]
 	[Range(0, 100)]
+	[Tooltip("Higher values means more likely to get spawned")]
 	private int spawnChance;
 	public int SpawnChance { get => spawnChance; private set => spawnChance = value; }
 
