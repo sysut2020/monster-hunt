66a9b349a18c2357a0ea0ca3dc76fc2126301b1c
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-23 18:36:15 +0100

    fixed comments

--- unityfiles/Assets/Scripts/GUI/Lettergame/LetterTile.cs
+++ unityfiles/Assets/Scripts/GUI/Lettergame/LetterTile.cs
@@ -6,7 +6,6 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class LetterTile : Dragable {
-
     [SerializeField]
     private TMP_Text LetterCount;
 
@@ -20,9 +19,7 @@ public class LetterTile : Dragable {
 
     // -- properties -- //
     public string LetterTileLetter {
-        get {
-            return letterTileLetter;
-        }
+        get => letterTileLetter;
         set {
             this.letterTileLetter = value;
             this.IconLetter = value;
@@ -33,55 +30,50 @@ public class LetterTile : Dragable {
     // -- events -- // 
 
     /// <summary>
-    /// Calback for the letter count change event
+    /// Callback for the letter count change event
     /// </summary>
+    /// <param name="_"></param>
     /// <param name="args">event args</param>
     private void CallbackLetterCountChangedEvent(object _, LetterCountChangedEventArgs args) {
-        Dictionary<string, int> lettercounts = args.AvailLetters;
+        Dictionary<string, int> letterCounts = args.AvailLetters;
 
-        if (lettercounts.ContainsKey(LetterTileLetter)) {
-            LetterCount.text = lettercounts[LetterTileLetter].ToString();
+        if (letterCounts.ContainsKey(LetterTileLetter)) {
+            LetterCount.text = letterCounts[LetterTileLetter].ToString();
         }
     }
 
     // -- private -- // 
 
     /// <summary>
-    /// Tels whether or not the Ui element should start the drag operation 
+    /// Tells whether or not the Ui element should start the drag operation 
     /// </summary>
     /// <returns>true if the drag operation can start false if not</returns>   
     override protected bool CanStartDrag() {
         holdingLetter = LetterGameManager.Instance.TryGetLetter(IconLetter);
         return holdingLetter != null;
-
     }
 
     /// <summary>
-    /// What the Ui element should do when the drag operation is complete
+    /// What the UI element should do when the drag operation is complete
     /// </summary>
     override protected void OnDragCompletion(PointerEventData eventData) {
-
         GameObject hit = eventData.pointerCurrentRaycast.gameObject;
         if (holdingLetter == null || hit == null) {
-            return; 
+            return;
         }
 
         if (hit.TryGetComponent<GameBoardTile>(out GameBoardTile tile)) {
             tile.SetTile(holdingLetter);
             holdingLetter = null;
         }
-
     }
     // -- unity -- //
 
-    // Start is called before the first frame update
     void Awake() {
         LetterGameManager.LetterCountCangedEvent += CallbackLetterCountChangedEvent;
-
     }
 
     private void OnDestroy() {
         LetterGameManager.LetterCountCangedEvent -= CallbackLetterCountChangedEvent;
     }
-
 }
