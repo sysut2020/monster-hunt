6aca69291c9719929a0927bb223496cf3d9fc270
Emil Nilsen <emil.nilsen9888@gmail.com>
2020-03-24 01:56:38 +0100

    Add rest of state holders

---
+++ unityfiles/Assets/Scripts/Helper/State holders/PauseMenuStateHolder.cs
@@ -0,0 +1,16 @@
+﻿public enum PAUSE_MENU_STATE {
+    /// <summary>
+    /// Waiting for player confirmation state
+    /// </summary>
+    CONFIRMATION,
+ 
+    /// <summary>
+    /// The base state of the pause menu
+    /// </summary>
+    BASE,
+ 
+    /// <summary>
+    /// Quiting state of pause menu
+    /// </summary>
+    QUIT
+}
--- unityfiles/Assets/Scripts/Helper/State holders/StateHolder.cs
+++ unityfiles/Assets/Scripts/Helper/State holders/StateHolder.cs
@@ -1,38 +1,9 @@
 using System;
-
-
-
-
-
-/// <summary>
-/// 
-/// </summary>
-public enum PLAYER_ANIMATION {
-
-    /// <summary>
-    /// 
-    /// </summary>
-    HOLD_SNIPER = 1,
-
-    /// <summary>
-    /// 
-    /// </summary>
-    HOLD_RAYGUN = 2
-}
-
-public enum PAUSE_MENU_STATE {
-    /// <summary>
-    /// Waiting for player confirmation state
-    /// </summary>
-    CONFIRMATION,
-
-    /// <summary>
-    /// The base state of the pause menu
-    /// </summary>
-    BASE,
-
-    /// <summary>
-    /// Quiting state of pause menu
-    /// </summary>
-    QUIT
-}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
