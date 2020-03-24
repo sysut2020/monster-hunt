1755c3b8e69b562a32a099b32da7b720a28d8e66
Emil Nilsen <emil.nilsen9888@gmail.com>
2020-03-24 01:51:16 +0100

    Add game state holder script

--- unityfiles/Assets/Scripts/Helper/State holders/StateHolder.cs
+++ unityfiles/Assets/Scripts/Helper/State holders/StateHolder.cs
@@ -1,29 +1,6 @@
 using System;
 
-/// <summary>
-/// The constants for the different game states
-/// </summary>
-public enum GAME_STATE {
-    /// <summary>
-    /// The Game should exit
-    /// </summary>
-    EXIT,
 
-    /// <summary>
-    /// Go to main menu
-    /// </summary>
-    MAIN_MENU,
-
-    /// <summary>
-    /// Go to the test level
-    /// </summary>
-    TEST_LEVEL,
-
-    /// <summary>
-    /// Go to the letter level
-    /// </summary>
-    LETTER_LEVEL
-}
 
 /// <summary>
 /// The constants for the different level states
