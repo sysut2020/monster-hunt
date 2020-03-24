3b164c93172452eb80479fe67a16a184262f7a3b
Emil Nilsen <emil.nilsen9888@gmail.com>
2020-03-24 01:52:43 +0100

    Add level state holder script

--- unityfiles/Assets/Scripts/Helper/State holders/StateHolder.cs
+++ unityfiles/Assets/Scripts/Helper/State holders/StateHolder.cs
@@ -2,48 +2,7 @@ using System;
 
 
 
-/// <summary>
-/// The constants for the different level states
-/// </summary>
-public enum LEVEL_STATE {
 
-    /// <summary>
-    /// Exit the current level
-    /// </summary>
-    EXIT,
-
-    /// <summary>
-    /// Start the play mode in game
-    /// </summary>
-    PLAY,
-
-    /// <summary>
-    /// Game pause
-    /// </summary>
-    PAUSE,
-
-    /// <summary>
-    /// Go to game over screen
-    /// </summary>
-    GAME_OVER,
-
-    /// <summary>
-    /// Start the hunting mode in game
-    /// </summary>
-    HUNTING,
-
-    /// <summary>
-    /// Reloads the current scene
-    /// </summary>
-    RELOAD,
-
-    GAME_WON,
-
-    /// <summary>
-    /// State to be ran at start of level
-    /// </summary>
-    START
-}
 
 /// <summary>
 /// 
