c4ee4851b044cc5e9659a9ce057f3f49a5541c60
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-24 15:17:18 +0100

    Implmented and started using the CellSizeCalculator

--- unityfiles/Assets/Scripts/GUI/Lettergame/PlayingBoardGUI.cs
+++ unityfiles/Assets/Scripts/GUI/Lettergame/PlayingBoardGUI.cs
@@ -1,4 +1,4 @@
-using System;
+using Helper;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,52 +19,37 @@ namespace GUI.Lettergame {
         private int cellSize;
 
         private float scaleFactor;
+        CellSizeCalculator calculator;
 
         [SerializeField]
         private GameObject boardTile;
 
-        private int tempRows; 
+        private int tempRows;
+
         private void Start() {
             gridLayout = gameObject.GetComponent<GridLayoutGroup>();
             playboardCanvas = gameObject.GetComponent<RectTransform>();
-            var rect = playboardCanvas.rect;
-            float height = rect.height;
-            // float height = gridLayout;
-            float width = rect.width;
 
-            RectOffset gridLayoutPadding = gridLayout.padding;
-            int horizontalPadding = gridLayoutPadding.horizontal;
-            int verticalPadding = gridLayoutPadding.vertical;
-
-            cellSizeX = Mathf.RoundToInt((width - horizontalPadding) / boardSizeX);
-            cellSizeY = Mathf.RoundToInt((height - verticalPadding) / boardSizeY);
-
-            cellSize = Math.Min(cellSizeX, cellSizeY);
-
-            // scaleFactor = cellSize / (float) orgBoardTileCellSize;
+            calculator = new CellSizeCalculator(playboardCanvas, boardSizeY, boardSizeX);
+            cellSize = calculator.GetOptimalCellSizeWidth();
             gridLayout.cellSize = new Vector2(cellSize, cellSize);
-
-            tempRows = Mathf.FloorToInt((height - verticalPadding) / cellSize);
 
             FillPlayingBoard();
         }
 
         void FillPlayingBoard() {
-            for (int i = 0; i <  
-                            boardSizeX * tempRows
-                ; i++) {
+            var rows = calculator.GetOptimalAmountOfRows();
+            var columns = boardSizeX;
+
+            for (int i = 0; i < columns * rows; i++) {
                 GameObject n = Instantiate(this.boardTile);
                 GameBoardTile tile = n.GetComponent<GameBoardTile>();
                 var rectTransform = n.GetComponent<RectTransform>();
-                var rect = rectTransform.rect;
-                // rect.height = cellSize;
-                // rect.width = cellSize;
-                
-                
-                tile.XPos = i % Mathf.RoundToInt(boardSizeX);
-                tile.YPos = (int) Mathf.Floor(i / boardSizeX);
+
+                tile.XPos = i % Mathf.RoundToInt(columns);
+                tile.YPos = (int) Mathf.Floor(i / columns);
                 n.transform.SetParent(transform);
-                rectTransform.localScale = new Vector3(1,1, 1);
+                rectTransform.localScale = new Vector3(1, 1, 1);
             }
         }
     }
