36ce260fe1b21d42f9307bcc4bfbc634679d5fe2
trygve@stasjoner <blomsterpotte1998@gmail.com>
2020-03-28 15:48:28 +0100

    added colectible managers

---
+++ unityfiles/Assets/Scripts/Game/Collectables/Pickups/LivesManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OnPickupLivesUpdateArgs : EventArgs {
+	public int LivesToAdd { get; set; }
+}
+
+
+public class LivesManager : MonoBehaviour{
+
+    [SerializeField]
+    private int livesValue = 2;
+    public static event EventHandler<OnPickupLivesUpdateArgs> OnPickupLivesUpdate;
+
+    // -- events -- //
+    private void SubscribeToEvents() {
+        PowerupCollectable.OnPowerupCollected += CallbackOnPowerupCollected;
+    }
+
+    private void UnsubscribeFromEvents() {
+        PowerupCollectable.OnPowerupCollected -= CallbackOnPowerupCollected;
+    }
+
+    private void CallbackOnPowerupCollected(object _, PowerUpCollectedArgs args) {
+        if (PICKUP_TYPE.LIVE == args.Effect){
+            var argsss = new OnPickupLivesUpdateArgs();
+            argsss.LivesToAdd = livesValue;
+            OnPickupLivesUpdate?.Invoke(this, argsss);
+        }
+    }
+
+
+
+
+
+    private void Awake() {
+        this.SubscribeToEvents();
+    }
+
+    private void OnDestroy() {
+        this.UnsubscribeFromEvents();
+    }
+}
