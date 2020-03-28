37e3d1ea9bc935cb73e6e89aabe13f0bbaad7e52
trygve@stasjoner <blomsterpotte1998@gmail.com>
2020-03-28 16:39:28 +0100

    fixed bug

--- unityfiles/Assets/Scripts/Core/LevelManager.cs
+++ unityfiles/Assets/Scripts/Core/LevelManager.cs
@@ -134,18 +134,22 @@ public class LevelManager : Singleton<LevelManager> {
             // The game is over show game over screen
             case LEVEL_STATE.GAME_OVER:
                 Time.timeScale = PAUSE;
+                this.levelTimer.Pause(this.LEVEL_TIMER_ID);
                 gameOverCanvas.SetActive(true);
                 break;
             case LEVEL_STATE.GAME_WON:
                 Time.timeScale = PAUSE;
+                this.levelTimer.Pause(this.LEVEL_TIMER_ID);
                 gameWonCanvas.SetActive(true);
                 break;
 
             case LEVEL_STATE.PAUSE:
                 Time.timeScale = PAUSE;
+                this.levelTimer.Pause(this.LEVEL_TIMER_ID);
                 break;
 
             case LEVEL_STATE.START:
+                this.levelTimer.Contue(this.LEVEL_TIMER_ID);
                 InitLevel();
                 ChangeLevelState(LEVEL_STATE.PLAY);
                 break;
@@ -179,7 +183,7 @@ public class LevelManager : Singleton<LevelManager> {
     /// spawns all the enemys and inits the player
     /// </summary>
     private void InitLevel() {
-        EntitySpawner.Instance.MaxSpawns = this.levelDetails.NumberOfEnemies;
+        EntitySpawner.Instance.MaxSpawns = this.levelDetails.NumberOfEnemies + 10;
         EntitySpawner.Instance?.Init(5); // Init with 5 mobs on the map
     }
 
