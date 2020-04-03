d9b493b8760ced80e2c36ec48a8765bacd628b41
Sander Hurlen <sanderhur1@gmail.com>
2020-04-03 14:37:08 +0200

    ADD Scoreboard player name screen + folder and scripts to prefab
    - Changes to scoreboard scene (ADD player name screen, background to canvas and deactive scoreboard GO)
    - New folder for storing UserNameGUI
    - Prefabs to UsernameGUI
    - Scripts for saving user name data to GameDataManager

---
+++ unityfiles/Assets/Scripts/GUI/Scoreboard/UserNameGUI/ScoreDisplayGroupGUIController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreDisplayGroupGUIController : MonoBehaviour {
+    [SerializeField]
+    private TextMeshProUGUI score;
+    void Start() {
+        if (score == null) {
+            throw new MissingComponentException("Missing TextMeshPro score component");
+        }
+        score.text = GameManager.Instance.GameDataManager.GameScore.ToString();
+    }
+}
---
+++ unityfiles/Assets/Scripts/GUI/Scoreboard/UserNameGUI/UserNameGUIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UserNameGUIController : MonoBehaviour {
+    [SerializeField]
+    private TextMeshProUGUI input;
+    private GameDataManager dataManager;
+    void Awake() {
+        if (input == null) {
+            throw new MissingComponentException("Missing TextMeshPro name component");
+        }
+    }
+
+    void Start() {
+        dataManager = GameManager.Instance.GameDataManager;
+    }
+
+    public void SaveHighScoreName() {
+        dataManager.HighScoreName = input.text;
+        dataManager.SaveData();
+    }
+}
