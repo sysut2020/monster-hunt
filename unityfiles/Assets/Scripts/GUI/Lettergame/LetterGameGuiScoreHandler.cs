35f6fc7a66d400f7f9e0701ffcacb8b9232b3adf
Christoffer A Traeen <christoffer.andersen@live.com>
2020-04-03 13:13:09 +0200

    Add score to lettergame
    - Show total game score
    - Show total words score
    - Moved play next level button

---
+++ unityfiles/Assets/Scripts/GUI/Lettergame/LetterGameGuiScoreHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class LetterGameGuiScoreHandler : MonoBehaviour {
+
+    [SerializeField]
+    private TMP_Text currentScoreText;
+
+    [SerializeField]
+    private TMP_Text wordScoreText;
+
+    private int currentWordScore = 0;
+
+    void Awake() {
+        if (this.currentScoreText == null) { throw new MissingComponentException("Add current score text component"); }
+        if (this.wordScoreText == null) { throw new MissingComponentException("Add word score text component"); }
+        LetterGameManager.OnWordScoreUpdateEvent += UpdateWordsScore;
+    }
+
+    private void Start() {
+        this.currentScoreText.SetText(GameManager.Instance.GameDataManager.GameScore.ToString());
+        this.wordScoreText.SetText(this.currentWordScore.ToString());
+    }
+
+    private void OnDestroy() {
+        LetterGameManager.OnWordScoreUpdateEvent -= UpdateWordsScore;
+    }
+
+    private void UpdateWordsScore(object _, WordScoreUpdateArgs args) {
+        int newScore = args.Score;
+
+        if (newScore != currentWordScore || newScore != 0) {
+            StartCoroutine(UpdateScoreText(newScore - currentWordScore));
+        }
+    }
+
+    /// <summary>
+    /// Incrementaly increase/decrease the score value> counter effect
+    /// </summary>
+    /// <param name="diff"></param>
+    /// <returns></returns>
+    private IEnumerator UpdateScoreText(int diff) {
+        bool countDown = diff < 0;
+        diff = Math.Abs(diff);
+        for (int i = 0; i < diff; i++) {
+            yield return new WaitForSeconds(.02f);
+            this.currentWordScore += countDown ? -1 : 1;
+            this.wordScoreText.SetText(this.currentWordScore.ToString());
+        }
+    }
+}
