d4c1b4cb41702a1bf0289fe32d7fe856b54b7b2e
Sander Hurlen <sanderhur1@gmail.com>
2020-04-06 08:45:43 +0200

    class comments

--- unityfiles/Assets/Scripts/GUI/Scoreboard/UserNameGUI/ScoreDisplayGroupGUIController.cs
+++ unityfiles/Assets/Scripts/GUI/Scoreboard/UserNameGUI/ScoreDisplayGroupGUIController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// Automatically updates the score text to the latest value from the data manager.
+/// </summary>
 public class ScoreDisplayGroupGUIController : MonoBehaviour {
     [SerializeField]
     private TextMeshProUGUI score;
