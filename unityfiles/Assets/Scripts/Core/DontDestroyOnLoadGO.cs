d09ff99b1fbb6f4a40d4dbff7325d152e7f85044
Christoffer A Traeen <christoffer.andersen@live.com>
2020-04-18 17:39:40 +0200

    Make DontDestroOnLoad start on Awake

--- unityfiles/Assets/Scripts/Core/DontDestroyOnLoadGO.cs
+++ unityfiles/Assets/Scripts/Core/DontDestroyOnLoadGO.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 /// <summary>
 /// Place on a game object To make it survive over scene changes
 /// </summary>
-public class DontDestroyOnLoadGO : MonoBehaviour{
+public class DontDestroyOnLoadGO : MonoBehaviour {
 
     public static GameObject Instance;
 
-    void Start(){
-        if(DontDestroyOnLoadGO.Instance != this.gameObject && DontDestroyOnLoadGO.Instance != null){
-            GameObject.Destroy(this.gameObject);   
-        }else{
+    void Awake() {
+        if (DontDestroyOnLoadGO.Instance != this.gameObject && DontDestroyOnLoadGO.Instance != null) {
+            GameObject.Destroy(this.gameObject);
+        } else {
             DontDestroyOnLoadGO.Instance = this.gameObject;
             DontDestroyOnLoad(this.gameObject);
         }
